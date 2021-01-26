95e19b3e0aaacada9caf4eedabad5b3344e3949d
Hajime Fujita <hajime.fujita@gmail.com>
2021-01-26 12:39:31 +0000

    CSS全然分からん

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <header class="siteHeader">
-    <div class="siteHeader__row">
-      <div class="siteHeader__wrapper">
-        <div class="siteHeader__nav-group">
-          <a href="/" class="siteHeader__logo">
-            <h1><img alt="Octave" src="@/assets/octave_logo.png"></h1>
-          </a>
-        </div>
-        <div class="siteHeader__nav-group">
-          <nav class="siteHeader__nav">
-            <ul class="siteHeader__nav-top-links">
-              <li class="siteHeader__nav-top-links-items siteHeader-button button hidden-logged-in">無料で登録</li>
-              <li class="siteHeader__nav-top-links-items"><router-link to="/">Home</router-link></li> |
-              <li class="siteHeader__nav-top-links-items"><router-link to="/about">About</router-link></li>
-            </ul>
-          </nav>	
+  <header class="main-header">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="over-all-top-heading">
+            <a href="/"><img alt="Octave" src="@/assets/octave_logo.png"></a>
+          </h1>
+          <div class="main-header__menu">
+            <nav class="top-links-nav visible-lg visible-md">
+              <ul class="top-links-list">
+                <li class="top-links-list__item siteHeader-button button hidden-logged-in">無料で登録</li>
+                <li class="top-links-list__item"><router-link to="/">Home</router-link></li> |
+                <li class="top-links-list__item"><router-link to="/about">About</router-link></li>
+              </ul>
+            </nav>	
+          </div>
         </div>
       </div>
     </div>
@@ -23,6 +23,170 @@
 </template>
 
 <style scoped>
+  
+  @media (min-width: 768px)
+  .main-header {
+  background-color: #fff;
+  }
+
+  .main-header {
+  background: #fff;
+  margin-bottom: 0;
+  color: #fff;
+  border-bottom: 0;
+  }
+
+  .main-header {
+  font-size: 14px;
+  letter-spacing: .2px;
+  font-weight: 400;
+  }
+
+  .main-header {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  }
+
+  .container-fluid {
+  position: relative;
+  z-index: 1;
+  }
+
+  @media (min-width: 768px)
+  .container-fluid {
+  padding-left: 30px;
+  padding-right: 30px;
+  }
+
+  .container-fluid {
+  margin-right: auto;
+  margin-left: auto;
+  }
+
+  .container-fluid:before {
+  display: table;
+  content: "";
+  }
+
+  .container-fluid:after {
+  clear: both;
+  }
+
+  .container-fluid:after {
+  display: table;
+  content: "";
+  }
+  
+  @media (min-width: 768px)
+  .row {
+  margin-left: -30px;
+  margin-right: -30px;
+  }
+
+  .row:before {
+  display: table;
+  content: "";
+  }
+
+  .row:after {
+  clear: both;
+  }
+
+  @media (min-width: 768px)
+  .col-md-12 {
+  height: 70px;
+  padding-left: 30px;
+  padding-right: 30px;
+  }
+
+  .col-md-12 {
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  }
+
+  .col-md-12 {
+  padding: 0;
+  }
+  
+  @media (min-width: 992px)
+  .col-md-12 {
+  width: 100%;
+  }
+
+  @media (min-width: 992px)
+  .col-md-12 {
+  float: left;
+  }
+
+  .col-md-12 {
+  position: relative;
+  min-height: 1px;
+  }
+
+  over-all-top-heading {
+  display: inline-block;
+  margin: 0;
+  }
+
+  .main-header__menu {
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  }
+
+  .top-links-nav {
+  display: block;
+  }
+  
+  @media (min-width: 1200px)
+  .visible-lg {
+  display: block !important;
+  }
+
+  .top-links-list {
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  margin-bottom: 0;
+  padding-left: 0;
+  list-style: none;
+  font-size: 0;
+  line-height: 0;
+  }
+
+  nav {
+  display: block;
+  }
+  
+  ul {
+  margin-top: 0;
+  }
+
+  ul {
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 40px;
+  }
+
+  .top-links-list__item {
+  display: inline-block;
+  position: relative;
+  }
+  
+  *:before, *:after {
+  box-sizing: border-box;
+  }
+  
+  * {
+  box-sizing: border-box;
+  }
+  
   #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -31,96 +195,6 @@
   color: #2c3e50;
   }
   
-  .siteHeader {
-  justify-content: space-between;
-  transition: padding 225ms, transform .3s ease-out;
-  width: 100%;
-  z-index: 900;
-  box-shadow: none;
-  top: 0;
-  position: fixed;
-  margin: 0 auto;
-  padding: 0 auto;
-  }
-
-  .siteHeader__row {
-  height: 56px;
-  width: 100%;
-  position: relative;
-  z-index: 10;
-  }
-
-  .siteHeader__row:before {
-  z-index: -1;
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-  box-shadow: 0 2px 12px 0 rgba(36,50,66,.075);
-  background-color: #fff;
-  transition: opacity .3s ease-in-out;
-  }
-  
-  .siteHeader__wrapper {
-  height: 100%;
-  max-width: 1440px;
-  display: flex;
-  flex-wrap: nowrap;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  margin: 0 auto;
-  padding: 0 64px;
-  orverflow: hidden;
-  }
-  
-  .siteHeader__nav-group {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  align-items: center;
-  height: 100%;
-  }
-  
-  .siteHeader__logo {
-  display: block;
-  position: relative;
-  }
-
-  .siteHeader__logo:before {
-  content: "";
-  position: absolute;
-  border-radius: 3px;
-  width: calc(100% + 20px);
-  height: calc(100% + 20px);
-  top: -10px;
-  left: -10px;
-  background: #edf1f2;
-  z-index: -1;
-  opacity: 0;
-  transform: scale(1,1);
-  }
-
-  .siteHeader__nav {
-  display: flex;
-  -webkit-box-align: center;
-  align-items: center;
-  }
-  
-  .siteHeader__nav-top-links {
-  margin: 0 auto;
-  padding: 0 auto;
-  list-style: none;
-  }
-  
-  .siteHeader__nav-top-links-items {
-  display: inline-block;
-  position: relative;
-  }
-
   .siteHeader-button {
   color: #fff;
   background-color: #273240;
