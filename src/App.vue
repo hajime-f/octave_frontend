474d968121bfade2c3080ffc5d0882e07dd3ac5d
Hajime Fujita <hajime.fujita@gmail.com>
2021-01-28 12:51:39 +0000

    コンポーネント化を試みた

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,5 @@
 <template>
-  <header class="siteHeader">
-    <div class="siteHeader__row">
-      <div class="siteHeader__wrapper">
-        <div class="siteHeader__nav_group">
-          <a class="siteHeader__logo" href="/">
-            <img alt="Octave" src="@/assets/octave_logo.png">
-          </a>
-          <nav class="horizontalNavigation">
-            <ul class="horizontalnavigation__list">
-              <li class="horizontalnavigation__list-item"><router-link to="/">Home</router-link></li> |
-              <li class="horizontalnavigation__list-item"><router-link to="/about">About</router-link></li>
-            </ul>
-          </nav>
-        </div>
-        <div class="siteHeader__nav_group">
-          <div class="navigation__link navigation__contact-sales contact-sales">aaa</div>
-          <div class="navigation__link navigation__sign-in hidden-logged-in">bbb</div>
-          <div class="siteHeader-buttons hidden-device-mobile">
-            <div class="siteHeader-button button hidden-logged-in">無料で試す</div>
-          </div>
-	</div>
-      </div>
-    </div>
-  </header>
+  
   <router-view/>
 </template>
 
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="siteHeader">
+    <div class="siteHeader__row">
+      <div class="siteHeader__nav_group">
+        <h1 class="siteHeader__logo"><img alt="Octave" src="@/assets/octave_logo.png"></h1>
+      </div>
+      <div class="siteHeader__nav_group">
+        <nav class="horizontalNavigation">
+          <ul class="horizontalNavigation__list">
+            <li class="horizontalNavigation__list-item"><router-link to="/">Home</router-link></li> |
+            <li class="horizontalNavigation__list-item"><router-link to="/">About</router-link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  
+  .siteHeader {
+  display: flex;
+  width: 100%;
+  height: 100px;
+  background-color: #fff;
+  align-items: center;
+  }
+
+  .siteHeader__logo {
+  margin-right: auto;
+  }
+
+  .horizontalNavigation__list {
+  list-style: none;
+  display: inline-block;
+  padding: 10px;
+  }
+  
+  
+</style>
