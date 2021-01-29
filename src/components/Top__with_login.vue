f682ac72d534c360a425ffdc5d35125f7be808d2
Hajime Fujita <hajime.fujita@gmail.com>
2021-01-29 12:43:49 +0000

    with_loginを追加

--- src/components/Top__with_login.vue
+++ src/components/Top__with_login.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="hello">
-    <h2>without_login</h2>
+    <h2>with_login</h2>
 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Top__without_login',
+  name: 'Top__with_login',
 }
 </script>
 
